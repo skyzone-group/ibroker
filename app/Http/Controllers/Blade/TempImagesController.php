66136a49c9d316877c11ceea015b784e8be28991
nodirsattorov <nodirbek.sattorov.96@gmail.com>
2022-08-31 18:01:43 +0500

    rotate-image complated for jpg|jpeg|png

--- app/Http/Controllers/Blade/TempImagesController.php
+++ app/Http/Controllers/Blade/TempImagesController.php
@@ -30,23 +30,49 @@ class TempImagesController extends ResponseController
     //Rotating Image
     public function rotateImage(Request $request)
     {
+        /*
         //get image
         $image = TempImages::whereId($request->image_id)->first();
         $path = public_file_path() . $image->name;
-        $source = imagecreatefromjpeg($path);
-        dd('ok');
+        $source = imagecreatefrompng($path);
+
         $rotate = imagerotate($source, 270, 0);
         //get file extension
-        $file_extn   = substr($image->name, strrpos($image->name, '.') + 1);
-        $newFileName = getCurrentMicrotime() . '.' . $file_extn;
+        $fileType   = substr($image->name, strrpos($image->name, '.') + 1);
+        $newFileName = getCurrentMicrotime() . '.' . $fileType;
         //and save it on your server...
-        $res = imagejpeg($rotate,public_file_path() . $newFileName);
+        $res = imagepng($rotate,public_file_path() . $newFileName);
 
         if($res) {
             TempImages::whereId($request->image_id)->update(['name' => $newFileName]);
             unlink(public_file_path()  . $image->name);
             return self::successResponse('Successfully rotated');
         }
+        */
 
+        $image = TempImages::whereId($request->image_id)->first();
+        $path = public_file_path() . $image->name;
+        $fileType   = substr($image->name, strrpos($image->name, '.') + 1);
+        $newFileName = getCurrentMicrotime() . '.' . $fileType;
+
+        if($fileType == 'png'){
+            $source = imagecreatefrompng($path);
+            $rotate = imagerotate($source, 270, 0);
+            imagesavealpha($rotate, true);
+            $res = imagepng($rotate, public_file_path() . $newFileName);
+        }
+        if($fileType == 'jpg' || $fileType == 'jpeg'){
+            header('Content-type: image/jpeg');
+            $source = imagecreatefromjpeg($path);
+            $rotate = imagerotate($source, 270, 0);
+            $res = imagejpeg($rotate, public_file_path() . $newFileName);
+        }
+        imagedestroy($source);
+        imagedestroy($rotate);
+        if($res) {
+            TempImages::whereId($request->image_id)->update(['name' => $newFileName]);
+            unlink(public_file_path()  . $image->name);
+            return self::successResponse('Successfully rotated');
+        }
     }
 }
