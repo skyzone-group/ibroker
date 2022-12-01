1d06de580054fcce31afcefa735657209d624aa5
nodirsattorov <nodirbek.sattorov.96@gmail.com>
2022-12-01 16:00:07 +0500

    Notification methods added

--- app/Http/Controllers/Blade/NotificationController.php
+++ app/Http/Controllers/Blade/NotificationController.php
@@ -2,10 +2,34 @@
 
 namespace App\Http\Controllers\Blade;
 
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\ResponseController;
 use Illuminate\Http\Request;
+use App\Models\Notification;
 
-class NotificationController extends Controller
+class NotificationController extends ResponseController
 {
     //
+    public function getAll(Request $request){
+        $user_id = auth('sanctum')->user()->id;
+        $results = Notification::where(['owner' => $user_id, 'is_readed' => 0])->get()->all();
+        
+        $data['count'] = count($results);
+        $data['items'] = $results;
+
+        return self::successResponse($data);   
+    }
+
+    public function show($id){
+        $user_id = auth('sanctum')->user()->id;
+
+        $result = Notification::where(['id' => $id, 'owner' => $user_id])->get()->first();
+
+        if($result && !$result->is_readed){
+            Notification::where(['id' => $id, 'owner' => $user_id])
+                        ->update(['is_readed' => 1]);
+        }
+
+        if($result) return self::successResponse($result);
+        return self::errorResponse('Not found');
+    }
 }
